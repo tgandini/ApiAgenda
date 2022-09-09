1e17276003d4a964dcf9cb368b5bf76232e52220
Tomas Gandini <tomas.gandini@avalith.net>
2022-09-09 11:50:06 -0300

    creamos el main y el compose de docker agregamos .vscode al ignore

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+#saludar con nombre y apellido
+@app.get("/saludar/{nombre}/{apellido}")
+async def saludar(nombre: str, apellido: str):
+    return {"message": f"Hola {nombre} {apellido}!! ¿Cómo estás?"}
