b5fa2595b8a135fd73c6e80c8d95321f30938ae9
Tomas Gandini <tomas.gandini@avalith.net>
2022-09-09 16:02:52 -0300

    agregamos delete cascade en los contactos cuando eliminamos persona

--- Models/Persona.py
+++ Models/Persona.py
@@ -1,14 +1,13 @@
 from bbdd import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from Models import Contacto
 
 class Persona(Base):
     __tablename__ = 'personas'
     id = Column(Integer, primary_key=True)
     nombre = Column(String(50), nullable=False)
     apellido = Column(String(50), nullable=False)
-    contactos = relationship("Contacto", back_populates="persona")
+    contactos = relationship("Contacto", back_populates="persona", cascade="all, delete-orphan")
 
     def __init__(self, nombre, apellido):
         self.nombre = nombre
