83c432a09eb540d5242280a5700d3c65cd9a4101
Tomas Gandini <tomas.gandini@avalith.net>
2022-09-09 15:52:25 -0300

    agregados metodos Modelo Persona

--- Models/Persona.py
+++ Models/Persona.py
@@ -1,18 +1,20 @@
 from bbdd import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
-
-tablaContactosPersonas = Table(
-    "personas_contactos",
-    Base.metadata,
-    Column("persona_id", ForeignKey("personas.id"), primary_key=True),
-    Column("contacto_id", ForeignKey("contactos.id"), primary_key=True),
-)
+from Models import Contacto
 
 class Persona(Base):
     __tablename__ = 'personas'
     id = Column(Integer, primary_key=True)
     nombre = Column(String(50), nullable=False)
     apellido = Column(String(50), nullable=False)
-    contactos = relationship("Contacto", secondary=tablaContactosPersonas, back_populates="personas")
-    
+    contactos = relationship("Contacto", back_populates="persona")
+
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.contactos=[]
+
+    def __repr__(self):
+        return f"{self.nombre} {self.apellido}. Contactos: {self.contactos}"
+        
